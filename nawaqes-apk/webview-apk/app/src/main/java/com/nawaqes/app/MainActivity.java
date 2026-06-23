ecc24a9d3fbb2e1d2261ce698a4f8f4be7b61c3c
Z User <z@container>
2026-06-23 17:21:25 +0000

    fix: Add missing Color import

--- nawaqes-apk/webview-apk/app/src/main/java/com/nawaqes/app/MainActivity.java
+++ nawaqes-apk/webview-apk/app/src/main/java/com/nawaqes/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
